8d9ba2be6b42e530300782b862107d4b485e0436
Yasin Yıldız <ysn.yldzzza@gmail.com>
2026-02-01 19:15:10 +0300

    end

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -2,7 +2,9 @@
 class ApiConfig {
   // ADB Reverse kullanıldığında (En kararlı yöntem)
   // Komut: adb reverse tcp:3000 tcp:3000
-  static const String baseUrl = 'http://127.0.0.1:3000/api';
+
+  static const String baseUrl =
+      'https://waspiest-flintiest-spencer.ngrok-free.dev/api';
 
   // Alternatif: Bilgisayarın yerel IP adresi (Eğer reverse çalışmazsa bunu deneyin)
   // static const String baseUrl = 'http://192.168.1.14:3000/api';
